ff3d6a922dcc0b6af76ae1e40d7d274316683504
harkirat75893 <66536770+harkirat75893@users.noreply.github.com>
2021-02-06 21:57:43 -0500

    Completed Lab 2

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>();
+            for (var i = 1; i < 4; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category" + i.ToString() });
+            }
+            return View(categories);
+        }
+        public IActionResult Details(string category) 
+        {
+            ViewBag.category = category;
+            return View();  
+        }
+        public IActionResult AddCategory() 
+        {
+            return View();
+        }
+    } 
+}
